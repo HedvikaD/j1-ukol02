f4987fc9ea03fea0b838318c1bbe2eee8a5fd7ec
Hedvika <hedvika.drechsler@seznam.cz>
2024-02-04 16:00:05 +0100

    část 1.1

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,6 +2,8 @@ package cz.czechitas.turtle;
 
 import dev.czechitas.java1.turtle.engine.Turtle;
 
+import java.awt.*;
+
 public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
@@ -11,6 +13,46 @@ public class HlavniProgram {
 
     public void start() {
         //TODO Tady bude kód pro kreslení želví grafiky.
-    }
+       nakresliRovnostrannyTrojuhelnik(100.0, Color.red);
+       nakresliCtvrerecek(200.0, Color.cyan);
+       nakresliObdelnik(150.0, 250, Color.black);
+       nakresliKolecko(20.0, new Color(130,250,105));
 
+    }
+    public void nakresliRovnostrannyTrojuhelnik (double velikostStrany, Color barva) {
+        zofka.setPenColor(barva);
+        for (int i = 0; i < 3; i++) {
+            zofka.move(velikostStrany);
+            zofka.turnLeft(120);
+        }
+    }
+    public void nakresliCtvrerecek (double velikostStrany, Color barva) {
+        zofka.setPenColor(barva);
+        for (int i = 0; i < 4; i++) {
+            zofka.move(velikostStrany);
+            zofka.turnLeft(90);
+        }
+    }
+    public void nakresliObdelnik (double velikostStranyA, double velikostStranyB, Color barva) {
+        zofka.setPenColor(barva);
+        for (int i = 0; i < 2; i++) {
+            zofka.move(velikostStranyA);
+            zofka.turnLeft(90);
+            zofka.move(velikostStranyB);
+            zofka.turnLeft(90);
+        }
+    }
+    public void nakresliKolecko (double velikostStrany, Color barva) {
+        zofka.setPenColor(barva);
+        for (int i = 0; i < 24; i++) {
+            zofka.move(velikostStrany);
+            zofka.turnLeft(15);
+
+        }
+
+
+
+
+
+    }
 }
